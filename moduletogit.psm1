f76c8c79e8fe80e4f11a41a6d8957b638da0a0a3
forzajuve83 <vzgurbanov@outlook.com>
2020-05-17 00:28:04 +0200

    systeminfo
    myfirst git module

---
+++ moduletogit.psm1
@@ -0,0 +1,13 @@
+﻿function Get-SystemInfo {
+[CmdletBinding()]
+param(
+[string[]]$ComputerName = "localhost",
+[string]$ErrorLog = "$HOME"+"\desktop\scripterror.txt"
+)
+BEGIN {}
+PROCESS {
+Write-Output $ComputerName
+Write-Output $ErrorLog
+}
+END {}
+}
