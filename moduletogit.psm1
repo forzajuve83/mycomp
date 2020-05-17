4d3d54b9a94106a7667fcb7a30666fd950b263dd
forzajuve83 <vzgurbanov@outlook.com>
2020-05-17 19:50:02 +0200

    systeminfo

---
+++ moduletogit.psm1
@@ -0,0 +1,15 @@
+﻿function Get-SystemInfo {
+[CmdletBinding()]
+param(
+[string[]]$ComputerName = "localhost",
+[string]$ErrorLog = "$HOME"+"\desktop\scripterror.txt"
+)
+BEGIN {}
+PROCESS {
+Write-Output $ComputerName
+Write-Output $ErrorLog
+Start-Sleep -Seconds 3
+Write-Host "That's all"
+}
+END {}
+}
